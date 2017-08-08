ab98353803dfd404d3815ca7f9b47b859abad4ac
Jon Ingram <jingram@classy.org>
2017-08-08 11:20:34 -0700

    Adding Pay client.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let Credstash = require('credstash-lambda')({
   region: Config.get('aws.region'),
   keys: _.keys(require(`${process.env.LAMBDA_TASK_ROOT}/creds.example.json`)),
   defaults: require('fs')
-  .existsSync(`${process.env.LAMBDA_TASK_ROOT}/creds.json`) ?
+    .existsSync(`${process.env.LAMBDA_TASK_ROOT}/creds.json`) ?
     require(`${process.env.LAMBDA_TASK_ROOT}/creds.json`) : null
 });
 let Common = {};
@@ -25,6 +25,12 @@ Common.load = next => {
         token: Config.get('LOG_LOGGLY_TOKEN'),
         subdomain: Config.get('log.loggly.subdomain'),
         tags: Config.get('log.loggly.tags')
+      });
+      Common.PayClient = require('./payClient')({
+        apiUrl: Config.get('pay.apiUrl'),
+        timeout: Config.get('pay.timeout'),
+        token: Config.get('PAY_TOKEN'),
+        secret: Config.get('PAY_SECRET')
       });
       next();
     }
---
+++ payClient.js
@@ -0,0 +1,52 @@
+'use strict';
+let apiUrl;
+let token;
+let secret;
+let timeout;
+let sign;
+
+function request(appId, method, resource, payload, callback) {
+  let options = {
+    url: `${apiUrl}${resource}?appId=${appId}&meta=true`,
+    timeout: timeout,
+    method,
+    headers: {
+      'Authorization': sign(method, resource, 'application/json',
+        payload ? JSON.stringify(payload) : null),
+      'User-Agent': 'ClassyPay Node.JS'
+    }
+  };
+  if (payload) {
+    options.headers['Content-Type'] = 'application/json';
+    options.body = JSON.stringify(payload);
+  }
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      callback(null, body ? JSON.parse(body) : {});
+    } else {
+      log.error(error || 'Response not 200: ' +
+        JSON.stringify(response, null, 2));
+      callback(error || 'Response was not 200 OK: ' +
+        JSON.stringify(response, null, 2));
+    }
+  });
+}
+
+module.exports = (config) => {
+  if (!config || !config.apiUrl || !config.timeout ||
+    !config.token || !config.secret) {
+    throw new Exception('You must provide apiUrl, timeout, token, and secret.');
+  }
+  apiUrl = config.apiUrl;
+  token = config.token;
+  secret = config.secret;
+  timeout = config.timeout;
+  sign = require('authorization-hmac256')({
+    service: 'CWS',
+    token,
+    secret
+  });
+  return {
+    request: request
+  };
+};
