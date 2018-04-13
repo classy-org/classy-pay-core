d5192afe4f33a650bcc2d8877142de0947cb1335
Jeremy Brown <jbrown@classy.org>
2018-04-13 11:17:26 -0700

    AWS handlers now support promises natively, so our wrapper can as well (and thus just handle bugsnag)

--- src/awsHandler.js
+++ src/awsHandler.js
@@ -2,21 +2,20 @@
 require('regenerator-runtime/runtime');
 require('source-map-support').install();
 const Common = require('./index');
-const _ = require('lodash');
 
 let bugsnag;
 
 module.exports = (handler) => {
-  return async (event, context, callback) => {
+  return async (event, context) => {
     if (!bugsnag) {
       bugsnag = await require('./utils/bugsnagFactory')();
     }
     try {
       let result = await handler(event, context, Common.async());
-      _.defer(callback, null, result);
+      return result;
     } catch (error) {
       bugsnag.notify(error);
-      _.defer(callback, error);
+      throw error;
     }
   };
 };
