5efafb21a6404320c6c397e55b12ea36aa07cca2
Jeremy Brown <jeremy@brownjava.org>
2018-06-21 10:06:55 -0700

    Go back to using callbacks with AWS, since their async/await support seems off (#14)

--- src/aws/handler.js
+++ src/aws/handler.js
@@ -2,6 +2,10 @@
 require('regenerator-runtime/runtime');
 require('source-map-support').install();
 
+const {promisify} = require('util');
+
+const Once = require('../utils/Once');
+
 const AWSConfig = require('./AWSConfig');
 const bugsnagFactory = require('../utils/bugsnagFactory');
 
@@ -9,20 +13,26 @@ const handlerGenerator = (handler, appName) => {
   let bugsnag;
   let config;
 
-  return async (event, context) => {
-    if (!config) {
-      config = new AWSConfig(appName);
-    }
-    if (!bugsnag) {
-      bugsnag = await bugsnagFactory(appName, await config.get('BUGSNAG_LAMBDAS_KEY'), await config.get('stage'));
-    }
-    try {
-      let result = await handler(event, context, config);
-      return result;
-    } catch (error) {
-      bugsnag.notify(error);
-      throw error;
-    }
+  let once = new Once(async () => {
+    config = new AWSConfig(appName);
+    bugsnag = await bugsnagFactory(appName, await config.get('BUGSNAG_LAMBDAS_KEY'), await config.get('stage'));
+  });
+
+  return (event, context, callback) => {
+    // We want our handlers to be async/await, but AWS cooperates better with callbacks
+    (async () => {
+      let result = null;
+      let error = null;
+      try {
+        await once.do();
+        result = await handler(event, context, config);
+      } catch (e) {
+        error = e.toString() === '[object Object]' ? new Error(JSON.stringify(e)) : e;
+        await promisify(bugsnag.notify)(error);
+      } finally {
+        callback(error, result);
+      }
+    })();
   };
 };
 
