9bc868ff9df85dba687317d01714583b419abd4e
Jeremy Brown <jbrown@classy.org>
2018-07-12 15:34:00 -0700

    Actually use APIClient in Config dict, and remove dependency on classy-api-client

--- src/DataSources/Clients.ts
+++ src/DataSources/Clients.ts
@@ -1,10 +1,10 @@
 require('source-map-support').install();
 
 const _ = require('lodash');
-import PayClient from '../PayClient';
-const Bluebird = require('bluebird');
 
-import {DataSource, DataSourceConfig} from '../DataSource';
+import { PayClient } from '../PayClient';
+import { APIClient } from '../APIClient';
+import { DataSource, DataSourceConfig } from '../DataSource';
 
 export class ClientDataSource extends DataSource {
   private clients?: { payClient?: PayClient, apiClient?: any };
@@ -22,13 +22,13 @@ export class ClientDataSource extends DataSource {
     }
 
     if (await config.get('api')) {
-      this.clients.apiClient = Bluebird.promisifyAll(require('classy-api-client')({
-        clientId: await config.get('APIV2_CLIENT_ID'),
-        clientSecret: await config.get('APIV2_CLIENT_SECRET'),
-        timeout: await config.get('api.timeout'),
-        oauthUrl: await config.get('api.oauthUrl'),
-        apiUrl: await config.get('api.apiUrl'),
-      }));
+      this.clients.apiClient = new APIClient(
+        await config.get('APIV2_CLIENT_ID'),
+        await config.get('APIV2_CLIENT_SECRET'),
+        await config.get('api.oauthUrl'),
+        await config.get('api.apiUrl'),
+        await config.get('api.timeout'),
+      );
     }
   }
 
